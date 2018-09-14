d2c7dfca24f10d8b3f12a4fa01b17f3b9c65ffc6
Aaron Nichols <anich003@gmail.com>
2018-09-14 18:22:50 +0000

    start DLList implementation

--- ch2/DLList.java
+++ ch2/DLList.java
@@ -1,3 +1,25 @@
-public class DLList {
-    
+public class DLList<T> {
+    private class Node {
+        public Node prev;
+        public T item;
+        public Node next;
+
+        public Node(T i, Node p, Node n) {
+            item = i;
+            next = n;
+            prev = p;
+        }
+    }
+
+    private int size;
+    private Node sentinel;
+
+    public DLList(T i) {
+        sentinel = new Node(null, null, null);
+        sentinel.next = new Node(i, null, null);
+    }
+
+    public static void main (String[] args) {
+        DLList<Integer> D = new DLList<>(5);
+    }
 }
