0a3e9b38deb3aed18a3de3635ed75c0fa7b52cce
Aaron Nichols <anich003@gmail.com>
2018-09-20 23:29:49 +0000

    add check_capacity_and_resize() to ArrayDeque

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -15,28 +15,58 @@ public class ArrayDeque<Item> {
 
     private int minusOne(int index) {
         int newIndex = index - 1;
-        return newIndex > 0 ? newIndex : items.length + newIndex;
+        return newIndex >= 0 ? newIndex : items.length + newIndex;
     }
 
     private int plusOne(int index) {
         int newIndex = index + 1;
-        return newIndex < items.length - 1 ? newIndex : newIndex - items.length;
+        return newIndex <= items.length - 1 ? newIndex : newIndex - items.length;
     }
 
+    /** Calculate starting point in dst array when copying over from src array 
+     * For example, when transitioning from an length 8 array to length 16,
+     * the dst array should start at position 4
+     * */
+    private int calculate_offset(int lengthSrc, int lengthDst) {
+        return (lengthDst - lengthSrc) / 2;
+    }
+
+    /** Determines if array needs to be resized to either a larger or smaller array.
+     * If 
+     * */
     private void check_capacity_and_resize() {
         if (size == items.length) {
             Item[] newItems = (Item[]) new Object[items.length * RFACTOR];
-            // TODO 
+            int newNextFirst = calculate_offset(items.length, newItems.length);
+
+            /** Copy over array starting from nextFirst to end... */
+            System.arraycopy(items, nextFirst, newItems, newNextFirst, items.length - nextFirst);
+            /** ...then copy over from start to nextFirst. */
+            if (nextLast < nextFirst) {
+                System.arraycopy(items, 0, newItems, items.length - nextFirst + newNextFirst, nextLast + 1);
+            }
+            
+            nextFirst = newNextFirst - 1;
+            nextLast = newNextFirst + items.length;
+            items = newItems;
+        }
+
+        if (size / items.length < UFACTOR) {
+            /** New smaller array */
+            Item[] newItems = (Item[]) new Object[items.length / 2];
+            
         }
     }
 
     public void addFirst(Item i) {
+        check_capacity_and_resize();
         items[nextFirst] = i;
         nextFirst = minusOne(nextFirst);
         size += 1;
     }
 
     public void addLast(Item i) {
+        check_capacity_and_resize();
         items[nextLast] = i;
         nextLast = plusOne(nextLast);
         size += 1;
@@ -56,6 +86,7 @@ public class ArrayDeque<Item> {
         items[nextFirst] = null;
         nextFirst = plusOne(nextFirst);
         size -= 1;
+        check_capacity_and_resize();
         return tmp;
     }
 
@@ -65,6 +96,7 @@ public class ArrayDeque<Item> {
         items[nextLast] = null;
         nextLast = minusOne(nextLast);
         size -= 1;
+        check_capacity_and_resize();
         return tmp;
     }
 
@@ -77,15 +109,15 @@ public class ArrayDeque<Item> {
         }
         return res;
     }
+
     public static void main(String[] args) {
         ArrayDeque<Integer> A = new ArrayDeque<>();
-        A.addFirst(10);
-        A.addLast(15);
-        System.out.println(A);
+        for (int i = 0; i < 1000000; i++) {
+            if (i%2 == 0) 
+                A.addFirst(i);
+            else
+                A.addLast(i);
+        }
         System.out.println(A.size());
-        System.out.println(A.removeFirst());
-        System.out.println(A.size());
-        System.out.println(A.removeLast());
-        System.out.println(A.removeLast());
     }
 }
