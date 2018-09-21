239820b7d4006b286d75458c718191572f92d04e
Aaron Nichols <anich003@gmail.com>
2018-09-21 22:26:09 +0000

    add JUnit annotations to SortTest

--- ch3/SortTest.java
+++ ch3/SortTest.java
@@ -1,42 +1,37 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 public class SortTest {
     
-    public static void testFindSmallestIndex() {
+    @Test
+    public void testFindSmallestIndex() {
         String[] input = {"i", "have", "an", "egg"};
         int expected = 2;
         int output = Sort.findSmallestIndex(input, 0);
-        org.junit.Assert.assertEquals(expected, output);
-        System.out.println("testFindSmallestIndex() : PASSED");
+        assertEquals(expected, output);
     }
 
-    public static void testSwap() {
+    @Test
+    public void testSwap() {
         String[] input = {"i", "have", "an", "egg"};
         Sort.swap(input,2,3);
         String[] expected = {"i", "have", "egg", "an"};
-        org.junit.Assert.assertArrayEquals(expected, input);
-        System.out.println("testSwap() : PASSED");
+        assertArrayEquals(expected, input);
     }
 
-    
-    public static void testSort() {
+    @Test
+    public void testSort() {
         String[] input = {"i", "have", "an", "egg"};
         String[] expected = {"an", "egg", "have", "i"};
         Sort.sort(input);
-        org.junit.Assert.assertArrayEquals(expected, input);
-        System.out.println("testSort() : PASSED");
+        assertArrayEquals(expected, input);
     }
 
-    public static void testSort2() {
+    @Test
+    public void testSort2() {
         String[] input = {"There", "were", "TWO", "werewolves","in","bed"};
         String[] expected = {"TWO","There","bed","in","were","werewolves"};
         Sort.sort(input);
-        org.junit.Assert.assertArrayEquals(expected, input);
-        System.out.println("testSort2() : PASSED");
-    }
-    
-    public static void main(String[] args) {
-        testFindSmallestIndex();
-        testSwap();
-        testSort();
-        testSort2();
+        assertArrayEquals(expected, input);
     }
 }
