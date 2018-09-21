60bf00999a291d9cd10f1a3d08e6cb2ebc57358e
Aaron Nichols <anich003@gmail.com>
2018-09-21 21:46:51 +0000

    complete Sort and SortTest in ch3

--- ch3/Sort.java
+++ ch3/Sort.java
@@ -1,8 +1,13 @@
 public class Sort {
 
-    public static int findSmallestIndex(String[] input) {
-        int smallestIndex = 0;
-        for (int i = 0; i < input.length; i++ ) {
+    /** Identifies the index of the smallest item in an String[] array
+     * after startIndex.
+     * @param input String[] array to be searched
+     * @param startIndex 
+     */
+    public static int findSmallestIndex(String[] input, int startIndex) {
+        int smallestIndex = startIndex;
+        for (int i = smallestIndex; i < input.length; i++ ) {
             if (input[i].compareTo(input[smallestIndex]) < 0) {
                 smallestIndex = i;
             }
@@ -10,9 +15,28 @@ public class Sort {
         return smallestIndex;
     }
 
+    /** Swaps two items in a String[] array */
+    public static void swap(String[] input, int i, int j) {
+        String tmp = input[i];
+        input[i] = input[j];
+        input[j] = tmp;
+    }
+
+    /** Recursive helper method for sorting an array starting at startIndex.
+     * Invariant: Items before startIndex are in sorted order.
+     * @param arr Array to be sorted
+     * @param startIndex 
+     */
+    private static void sort(String[] arr, int startIndex) {
+        if (startIndex != arr.length - 1) {
+            int smallestIndex = findSmallestIndex(arr, startIndex);
+            swap(arr, startIndex, smallestIndex);
+            sort(arr, startIndex+1);
+        }
+    }
+
+    /** Sorts the input String[] array using Selection-Sort */
     public static void sort(String[] arr) {
-        // Find smallest
-        // Swap itmes
-        // Recurse
+        sort(arr, 0);
     }
 }
--- ch3/SortTest.java
+++ ch3/SortTest.java
@@ -3,21 +3,40 @@ public class SortTest {
     public static void testFindSmallestIndex() {
         String[] input = {"i", "have", "an", "egg"};
         int expected = 2;
-        int output = Sort.findSmallestIndex(input);
+        int output = Sort.findSmallestIndex(input, 0);
         org.junit.Assert.assertEquals(expected, output);
         System.out.println("testFindSmallestIndex() : PASSED");
     }
 
-    /**
+    public static void testSwap() {
+        String[] input = {"i", "have", "an", "egg"};
+        Sort.swap(input,2,3);
+        String[] expected = {"i", "have", "egg", "an"};
+        org.junit.Assert.assertArrayEquals(expected, input);
+        System.out.println("testSwap() : PASSED");
+    }
+
+    
     public static void testSort() {
         String[] input = {"i", "have", "an", "egg"};
         String[] expected = {"an", "egg", "have", "i"};
         Sort.sort(input);
         org.junit.Assert.assertArrayEquals(expected, input);
+        System.out.println("testSort() : PASSED");
     }
-    */
 
+    public static void testSort2() {
+        String[] input = {"There", "were", "TWO", "werewolves","in","bed"};
+        String[] expected = {"TWO","There","bed","in","were","werewolves"};
+        Sort.sort(input);
+        org.junit.Assert.assertArrayEquals(expected, input);
+        System.out.println("testSort2() : PASSED");
+    }
+    
     public static void main(String[] args) {
         testFindSmallestIndex();
+        testSwap();
+        testSort();
+        testSort2();
     }
 }
