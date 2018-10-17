ef44b6256b81ae88026218b04fafd961dfad1e8f
Aaron Nichols <anich003@gmail.com>
2018-10-16 21:51:09 -0700

    draw room from location in direction

--- proj2/byog/Core/MapGenerator.java
+++ proj2/byog/Core/MapGenerator.java
@@ -23,6 +23,27 @@ public class MapGenerator {
 
     public TETile[][] generateRandomMap(int width, int height) {
         TETile[][] map = initializeMap(width, height);
+
+        map[width/2][height/2] = Tileset.FLOWER;
+        int roomWidth = 10;
+        int roomHeight = 8;
+        int x = getRoomLowerBound(width/2, roomWidth);
+        int y = getRoomLowerBound(height/2, roomHeight);
+
+        Room r = new Room(x, y, roomWidth, roomHeight);
+        r.addTo(map);
+
+
+        r = new Room(10, 10, 7, 9, Direction.Right);
+        r.addTo(map);
+        map[10][10] = Tileset.FLOWER;
+
+        map[13][14] = Tileset.FLOWER;
+        r = new Room(13, 14, 5, 11, Direction.Up);
+        r.addTo(map);
+
+        map[13][14] = Tileset.FLOOR;
+
         return map;
     }
 
@@ -35,9 +56,74 @@ public class MapGenerator {
         }
         return map;
     }
+
+    private enum Direction { Up, Down, Left, Right };
+
+    private int getRoomLowerBound(int center, int length) {
+        return center - length / 2;
+    }
+
+    private int getRoomUpperBound(int center, int length) {
+        return center + (length + 1) / 2;
+    }
+
+    private class Room {
+        private int xmin;
+        private int ymin;
+        private int width;
+        private int height;
+
+        public Room(int xmin, int ymin, int width, int height) {
+            this.xmin = xmin;
+            this.ymin = ymin;
+            this.width = width;
+            this.height = height;
+        }
+
+        public Room(int xCenter, int yCenter, int width, int height, Direction d) {
+            this.width = width;
+            this.height = height;
+
+            switch (d) {
+                case Up:
+                    this.xmin = getRoomLowerBound(xCenter, width);
+                    this.ymin = yCenter;
+                    break;
+                case Down:
+                    this.xmin = getRoomLowerBound(xCenter, width);
+                    this.ymin = yCenter - height;
+                    break;
+                case Left:
+                    this.xmin = xCenter - width;
+                    this.ymin = getRoomLowerBound(yCenter, height);
+                    break;
+                case Right:
+                    this.xmin = xCenter;
+                    this.ymin = getRoomLowerBound(yCenter, height);
+                    break;
+                default:
+                    throw new IllegalArgumentException("Invalid Direction");
+            }
+        }
+
+        public void addTo(TETile[][] map) {
+            for (int y = 0; y < height; y += 1) {
+                for (int x = 0; x < width; x += 1) {
+                    if (y == 0 || y == height - 1) {
+                        map[x + xmin][y + ymin] = Tileset.WALL;
+                    } else if (x == 0 || x == width - 1) {
+                        map[x + xmin][y + ymin] = Tileset.WALL;
+                    } else {
+                        map[x + xmin][y + ymin] = Tileset.FLOOR;
+                    }
+                }
+            }
+        }
+    }
+
     public static void main(String[] args) {
         int width = 80;
-        int height = 40;
+        int height = 30;
         MapGenerator mg = new MapGenerator(1234);
         TETile[][] map = mg.generateRandomMap(width, height);
 
